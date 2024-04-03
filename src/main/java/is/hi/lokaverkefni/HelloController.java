9e8e9eef983939946ad364e9164101e6185b9f37
sigga <ssj44@hi.is>
2024-04-03 12:32:32 +0000

    lala

--- src/main/java/is/hi/lokaverkefni/HelloController.java
+++ src/main/java/is/hi/lokaverkefni/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Lokaverkefni sem verður geggjað");
+        welcomeText.setText("Lokaverkefni sem verður geggjað kv Sigga");
     }
 }
