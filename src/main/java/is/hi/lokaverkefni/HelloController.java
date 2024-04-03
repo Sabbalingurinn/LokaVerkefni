aae9b1c7d8b0f6dce053a87f7fe2be8bcada5ee9
Sabbalingurinn <s.b.snorrason@gmail.com>
2024-04-03 12:31:17 +0000

    önnur prufa

--- src/main/java/is/hi/lokaverkefni/HelloController.java
+++ src/main/java/is/hi/lokaverkefni/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Lokaverkefni");
+        welcomeText.setText("Lokaverkefni sem verður geggjað");
     }
 }
