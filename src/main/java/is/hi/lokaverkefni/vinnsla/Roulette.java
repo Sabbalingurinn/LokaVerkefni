585aaa554709c5b4a4acc097ca4152b23bac5f06
Sabbalingurinn <s.b.snorrason@gmail.com>
2024-04-17 11:57:06 +0000

    javadoc

--- src/main/java/is/hi/lokaverkefni/vinnsla/Roulette.java
+++ src/main/java/is/hi/lokaverkefni/vinnsla/Roulette.java
@@ -2,13 +2,19 @@ package is.hi.lokaverkefni.vinnsla;
 
 public class Roulette {
 
-    private int[] toluVedmal = {-1, -1, -1};
-    private String[] rauttSvartVedmal = new String[3];
-    private int fjoldiBet = 0;
+    private int[] toluVedmal = {-1, -1, -1}; //Geymir töluleg veðmál notenda
+    private String[] rauttSvartVedmal = new String[3]; //Geymir veðmál notenda á svart og rautt
+    private int fjoldiBet = 0; //Fjoldi veðmála
     private final int[] RAUDARTOLUR = {32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3};
     private final int[] SVARTARTOLUR = {15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26};
-    private int sidastaTala;
+    private int sidastaTala; //Síðasta tala sem kom upp í snúningi
 
+    /**
+     * Framkvæmir snúning á rúllettuhjólinu og reiknar út mögulegan vinning eða tap.
+     *
+     * @param bet Fjöldi peninga sem veðjað er í hverju veðmáli.
+     * @return Heildarvinnings eða tapsupphæð eftir snúning.
+     */
     public int spin(int bet) {
 
         int mogulegurSigur = 0;
@@ -31,12 +37,20 @@ public class Roulette {
         return mogulegurSigur;
     }
 
+    /**
+     * Stillir aftur breytur sem tengjast veðmálum til að undirbúa fyrir næsta snúning.
+     */
     public void nullStilla(){
         setFjoldiBet(0);
         toluVedmal = new int[]{-1, -1, -1};
         rauttSvartVedmal = new String[3];
     }
 
+    /**
+     * Ákveður hvort síðasta tala var rauð, svört eða græn.
+     *
+     * @return Skilar staf 'R' fyrir rauða, 'B' fyrir svarta, 'G' fyrir græna.
+     */
     public String svortRaudTala(){
         for(int i = 0; i < RAUDARTOLUR.length; i++){
             if(RAUDARTOLUR[i] == this.sidastaTala){
@@ -51,24 +65,40 @@ public class Roulette {
         return "G";
     }
 
+    /**
+     * Bætir við veðmáli á rautt eða svart.
+     *
+     * @param svartRautt Strengur sem táknar lit veðmáls ('R' eða 'B').
+     */
     public void baetaVidSvartRauttBet(String svartRautt){
         rauttSvartVedmal[fjoldiBet] = svartRautt;
         fjoldiBet++;
     }
 
+    /**
+     * Bætir við veðmáli á ákveðna tölu.
+     *
+     * @param tala Tölulegt gildi veðmáls.
+     */
     public void baetaVidToluVedmal(int tala){
         toluVedmal[fjoldiBet] = tala;
         fjoldiBet++;
     }
 
+    /**
+     * Nær í síðustu tölu sem kom upp á rúllettuhjólinu.
+     *
+     * @return Tala sem datt út í síðasta snúningi.
+     */
     public int getSidastaTala() {
         return sidastaTala;
     }
 
-    public void setSidastaTala(int sidastaTala) {
-        this.sidastaTala = sidastaTala;
-    }
-
+    /**
+     * Stillir fjölda gerðra veðmála.
+     *
+     * @param i Nýr fjöldi veðmála.
+     */
     public void setFjoldiBet(int i){
         fjoldiBet = i;
     }
